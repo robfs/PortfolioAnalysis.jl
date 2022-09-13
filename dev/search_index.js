2caee9921e4062907d2d81ad0b193f0b574ed230
Documenter.jl <documenter@juliadocs.github.io>
2022-09-13 17:46:33 +0000

    build based on 89b88f4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PortfolioAnalysis","category":"page"},{"location":"#PortfolioAnalysis","page":"Home","title":"PortfolioAnalysis","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PortfolioAnalysis.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PortfolioAnalysis]","category":"page"}]
+}
