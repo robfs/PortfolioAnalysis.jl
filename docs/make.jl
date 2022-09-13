8b58ca07a6606ffe5bd8f283d28fdc5e803636e0
Rob <rsimms1990@gmail.com>
2022-09-13 19:08:06 +0100

    formatting

--- docs/make.jl
+++ docs/make.jl
@@ -1,25 +1,25 @@
 using PortfolioAnalysis
 using Documenter
 
-DocMeta.setdocmeta!(PortfolioAnalysis, :DocTestSetup, :(using PortfolioAnalysis); recursive=true)
+DocMeta.setdocmeta!(
+    PortfolioAnalysis,
+    :DocTestSetup,
+    :(using PortfolioAnalysis);
+    recursive = true,
+)
 
 makedocs(;
-    modules=[PortfolioAnalysis],
-    authors="robfs <robfoxsimms@gmail.com> and contributors",
-    repo="https://github.com/robfs/PortfolioAnalysis.jl/blob/{commit}{path}#{line}",
-    sitename="PortfolioAnalysis.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://robfs.github.io/PortfolioAnalysis.jl",
-        edit_link="main",
-        assets=String[],
+    modules = [PortfolioAnalysis],
+    authors = "robfs <robfoxsimms@gmail.com> and contributors",
+    repo = "https://github.com/robfs/PortfolioAnalysis.jl/blob/{commit}{path}#{line}",
+    sitename = "PortfolioAnalysis.jl",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://robfs.github.io/PortfolioAnalysis.jl",
+        edit_link = "main",
+        assets = String[],
     ),
-    pages=[
-        "Home" => "index.md",
-    ],
+    pages = ["Home" => "index.md"],
 )
 
-deploydocs(;
-    repo="github.com/robfs/PortfolioAnalysis.jl",
-    devbranch="main",
-)
+deploydocs(; repo = "github.com/robfs/PortfolioAnalysis.jl", devbranch = "main")
