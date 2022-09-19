c4dcb24f7999ad879e8ea03104d53dbb461d1853
Rob <rsimms1990@gmail.com>
2022-09-19 10:08:08 +0100

    add security docstrings

--- src/Securities.jl
+++ src/Securities.jl
@@ -1,3 +1,8 @@
+"""
+Securities
+
+This package provides the various `Security` types together with appropriate methods.
+"""
 module Securities
 
 using ..Countries, ..Currencies
@@ -16,13 +21,44 @@ export AbstractSecurity,
 
 abstract type AbstractSecurity end
 
+"""
+Returns the identifier of a security
+"""
 securityid(s::AbstractSecurity)::String = s.identifier
+
+"""
+Returns the name of a security
+"""
 securityname(s::AbstractSecurity)::String = s.name
+
+"""
+Returns the country of a security
+"""
 securitycountry(s::AbstractSecurity)::Type{Country{S}} where {S} = s.country
+
+"""
+Returns the currency of a security
+"""
 securitycurrency(s::AbstractSecurity)::Type{Currency{S}} where {S} = s.currency
+
+"""
+Returns the GICS subindustry number of a security
+"""
 subindustrynum(s::AbstractSecurity)::Int = s.gicsnum
+
+"""
+Returns the GICS industry number of a security
+"""
 industrynum(s::AbstractSecurity)::Int = subindustrynum(s) ÷ 100
+
+"""
+Returns the GICS industry group number of a security
+"""
 industrygroupnum(s::AbstractSecurity)::Int = industrynum(s) ÷ 100
+
+"""
+Returns the GICS sector number of a security
+"""
 sectornum(s::AbstractSecurity)::Int = industrygroupnum(s) ÷ 100
 
 abstract type PhysicalSecurity <: AbstractSecurity end
